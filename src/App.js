91274a843123a440fb7ae930054f1fece398997a
Rachel Geisman <rachel.geisman@protonmail.com>
2024-02-04 10:06:58 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAx8YqwRM_R1H3CEv7hLIyVtMyFH2TiiXI",
+  authDomain: "blaring-infrared-orchestra.firebaseapp.com",
+  projectId: "blaring-infrared-orchestra",
+  storageBucket: "blaring-infrared-orchestra.appspot.com",
+  messagingSenderId: "315861489073",
+  appId: "1:315861489073:web:6aa1ad8684cad958115990",
+  measurementId: "G-SZWEEFJMYB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
